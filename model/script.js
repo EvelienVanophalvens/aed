654bd6d3c26ebd7d464efa7265cbc560e1264276
Evelien Vanophalvens <Evelien.Vanophalvens@telenet.be>
2023-12-23 16:29:49 +0100

    Load the MobileNet base model

--- model/script.js
+++ model/script.js
@@ -24,6 +24,29 @@ for (let i = 0; i < dataCollectorButtons.length; i++) {
   CLASS_NAMES.push(dataCollectorButtons[i].getAttribute('data-name'));
 }
 
+
+/**
+ * Loads the MobileNet model and warms it up so ready for use.
+ **/
+async function loadMobileNetFeatureModel() {
+    const URL = 
+      'https://tfhub.dev/google/tfjs-model/imagenet/mobilenet_v3_small_100_224/feature_vector/5/default/1';
+    
+    mobilenet = await tf.loadGraphModel(URL, {fromTFHub: true});
+    STATUS.innerText = 'MobileNet v3 loaded successfully!';
+    
+    // Warm up the model by passing zeros through it once.
+    tf.tidy(function () {
+      let answer = mobilenet.predict(tf.zeros([1, MOBILE_NET_INPUT_HEIGHT, MOBILE_NET_INPUT_WIDTH, 3]));
+      console.log(answer.shape);
+    });
+}
+  
+// Call the function immediately to start loading.
+loadMobileNetFeatureModel();
+
+
+
 function gatherDataForClass() {
     // TODO: Fill this out later in the codelab!
 }
