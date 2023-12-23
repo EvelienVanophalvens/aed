036b3ef24f18c5c84e8304c31d8fc65564e62873
Evelien Vanophalvens <Evelien.Vanophalvens@telenet.be>
2023-12-23 16:22:31 +0100

    feat: setup the functions

--- model/script.js
+++ model/script.js
@@ -5,3 +5,15 @@ const MOBILE_NET_INPUT_WIDTH = 224;
 const MOBILE_NET_INPUT_HEIGHT = 224;
 const STOP_DATA_GATHER = -1;
 const CLASS_NAMES = [];
+
+TRAIN_BUTTON.addEventListener('click', trainAndPredict);
+RESET_BUTTON.addEventListener('click', reset);
+
+function trainAndPredict() {
+    // TODO: Fill this out later in the codelab!
+}
+
+
+function reset() {
+    // TODO: Fill this out later in the codelab!
+}
